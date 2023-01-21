7cfa897fe355e63f59b60e7b3ed0a66f81ce50a5
Kameliya Marinova <kameliyamarinova@gmail.com>
2023-01-21 15:15:31 +0200

    add more tests

--- Summator.UnitTests/SummatorTests.cs
+++ Summator.UnitTests/SummatorTests.cs
@@ -6,7 +6,37 @@ namespace Summator.UnitTests
 
         public void Test_Summator_SumTwoPositiveNumbers()
         {
+            var nums = new int[] { 1, 2 };
+            var actual = Summator.Sum(nums);
 
+            var expected = 4;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+
+        public void Test_Summator_SumTwoNegativeNumbers()
+        {
+            var nums = new int[] { -1, -99 };
+            var actual = Summator.Sum(nums);
+
+            var expected = -100;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+
+        public void Test_Summator_OneNumber()
+        {
+            var nums = new int[] { 6 };
+            var actual = Summator.Sum(nums);
+
+            var expected = 6;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
+ 
+    
+
