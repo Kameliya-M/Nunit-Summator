3b28eefc6f130dfe4c5c8a5923b2c510ff73d764
Kameliya Marinova <kameliyamarinova@gmail.com>
2023-01-21 12:54:02 +0200

    test 2

--- Summator/Program.cs
+++ Summator/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             Summator.Test_SumTwoNumbers();
-            //koookok
+            //ko
+            //boboboboobo
         }
     }
 }
